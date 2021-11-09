1b1494abfa41c3d182b89fbf7a0286cd11c95907
BarryFoye <barry.foye@gmail.com>
2021-11-09 21:25:53 +0000

    minor change to rmove unused import

--- src/repositories/market_data.ts
+++ src/repositories/market_data.ts
@@ -1,5 +1,5 @@
 import { getRepository } from 'typeorm';
-import { Coin, Quote } from '../models';
+import { Coin } from '../models';
 
 export interface IMarketDataPayload {
 	data: Coin;
@@ -9,3 +9,33 @@ export const getWeeklySnapshots = async (): Promise<Array<Coin>> => {
 	const marketDataRepository = getRepository(Coin);
 	return marketDataRepository.find();
 };
+
+// TODO: Test me!
+// export const dataTree = async (): Promise<unknown> => {
+// 	const quote = getRepository(Quote);
+// 	const totalMarketCap = quote
+// 		.createQueryBuilder('quote')
+// 		.select(['SUM(quote.market_cap)'])
+// 		.where('quote.currency = :currency', { currency: 'USD' })
+// 		.andWhere('') // TODO: Add logic to select only the latest data
+// 		.getRawOne();
+// 	const coinMarketValue = quote
+// 		.createQueryBuilder('quote')
+// 		.leftJoinAndSelect(Coin, 'coin', 'coin.id = quote.coin_id')
+// 		.select(['coin.name', 'quote.market_cap'])
+// 		.where('quote.currency = :currency', { currency: 'USD' })
+// 		.orderBy('quote.last_updated', 'DESC')
+// 		.addOrderBy('quote.market_cap', 'DESC')
+// 		.addOrderBy('coin.name', 'ASC')
+// 		.limit(10)
+// 		.getRawMany();
+
+// 	return {
+// 		name: 'Market Data',
+// 		children: {
+// 			name: 'Total Market Cap',
+// 			value: totalMarketCap,
+// 			children: coinMarketValue
+// 		}
+// 	};
+// };
